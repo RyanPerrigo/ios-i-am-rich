e60d4b0130bfdcbef5e20f209ff893a72eed72ba
imantium <ryan.perrigo@gmail.com>
2020-04-28 22:09:04 -0400

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ryan Perrigo on 4/28/20.
+//  Copyright © 2020 Ryan Perrigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
